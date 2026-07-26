26650c340cfb62f49cb4f813e5eeb236b74b7413
Mostafa Al Natsheh <mostafaalnatsheh53@gmail.com>
2026-07-26 19:08:48 +0300

    Complete generic repository lab

--- Week02/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ Week02/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93f7eb97e9ffd55363cfd8272f110b4069bcf40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff23aa15fbe4beab2ab5bfb79f3887f11758770")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
